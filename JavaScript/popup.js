3768a1199354ad655c9d0c8d7291221ed7afca19
Taynad <166086044+Taynad@users.noreply.github.com>
2025-01-16 23:56:07 -0300

    popup cadastrar produto

---
+++ JavaScript/popup.js
@@ -0,0 +1,15 @@
+const btn_addproduct = document.getElementById('btn_addProduct');
+const btn_cancel = document.getElementById('btn_cancel')
+const popup_register = document.getElementById('popup_registerProduct');
+const overlay = document.getElementById('overlay');
+
+//função para abrir o popup
+btn_addproduct.onclick = function(){
+    popup_register.style.display = "block";
+    overlay.style.display = "block";
+}
+
+btn_cancel.onclick = function(){
+    popup_register.style.display = "none";
+    overlay.style.display = "none";
+}
